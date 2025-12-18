020178fa99e163db47d08f874d8a15c5d2b52c0c
Saumya Palakodety <palakodety.s@northeastern.edu>
2025-12-18 11:11:33 +0530

    website

---
+++ script.js
@@ -0,0 +1,22 @@
+const text = "spalak";
+const speed = 50;
+let index = 0;
+
+const typeText = document.getElementById("type-text");
+const intro = document.getElementById("intro");
+const main = document.getElementById("main");
+
+function type() {
+  if (index < text.length) {
+    typeText.textContent += text.charAt(index);
+    index++;
+    setTimeout(type, speed);
+  } else {
+    setTimeout(() => {
+      intro.classList.add("slide-up");
+      main.classList.add("slide-up");
+    }, 600);
+  }
+}
+
+type();
